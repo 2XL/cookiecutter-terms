90789f53e07a0702710cba79e7cc855dda631bab
Cheng <chenglong.zq@gmail.com>
2019-05-02 09:00:56 +0200

    Updates

---
+++ precache-manifest.3c9fff4a7ca98bc119f05fea0ef7ae47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f15d2f5f88287c966d2",
+    "url": "/cookiecutter-terms/static/js/runtime~main.724931b5.js"
+  },
+  {
+    "revision": "d1e9e9f9842e3d07390b",
+    "url": "/cookiecutter-terms/static/js/main.9426891b.chunk.js"
+  },
+  {
+    "revision": "d3977cd5db9c04604a8e",
+    "url": "/cookiecutter-terms/static/js/2.f9c82145.chunk.js"
+  },
+  {
+    "revision": "d1e9e9f9842e3d07390b",
+    "url": "/cookiecutter-terms/static/css/main.764bcd90.chunk.css"
+  },
+  {
+    "revision": "d3977cd5db9c04604a8e",
+    "url": "/cookiecutter-terms/static/css/2.e7ba0d52.chunk.css"
+  },
+  {
+    "revision": "564c1a214477e9fa41ecc838e16d46ca",
+    "url": "/cookiecutter-terms/index.html"
+  }
+];
